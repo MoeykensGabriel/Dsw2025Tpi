c8ba3b826bf48cd2d013ffdb5ba2739173ce4c97
MoeykensGabriel <moeykens.gabriel@gmail.com>
2025-06-21 15:51:44 -0300

    Se modifico la entidad Product.cs

--- Dsw2025Tpi.Domain/Entities/Product.cs
+++ Dsw2025Tpi.Domain/Entities/Product.cs
@@ -6,15 +6,42 @@ using System.Threading.Tasks;
 
 namespace Dsw2025Tpi.Domain.Entities
 {
-    public class Product:EntityBase
+    public class Product : EntityBase
     {
-        public string Sku {  get; set; }
-        public string InternalCode { get; set; }
-        public string Name { get; set; }    
-        public string Description { get; set; }
-        public decimal CurrentUnitPrice { get; set; }
-        public int StockQuantity { get; set; }
-        public Boolean IsStock { get; set; }
+        public string? Sku { get; init; }
+        public string? InternalCode { get; private set; } 
+        public string? Name { get; private set; }
+        public string? Description { get; private set; }
+        public decimal CurrentUnitPrice { get; private set; }
+        public int StockQuantity { get; private set; }
+        public bool IsActive { get; private set; } = true;
 
+        public Product(string sku, string internalCode, string name, string description, decimal price, int stock)
+        {
+            if (string.IsNullOrWhiteSpace(sku))
+                throw new ArgumentException("El SKU es obligatorio.");
+
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("El nombre es obligatorio.");
+
+            if (price <= 0)
+                throw new ArgumentException("El precio debe ser mayor a 0.");
+
+            if (stock < 0)
+                throw new ArgumentException("El stock no puede ser negativo.");
+
+
+            Sku = sku;
+            InternalCode = internalCode;
+            Name = name;
+            Description = description;
+            CurrentUnitPrice = price;
+            StockQuantity = stock;
+            IsActive = true;
+        }
+        public Product() { }
+        
+        
     }
+
 }
