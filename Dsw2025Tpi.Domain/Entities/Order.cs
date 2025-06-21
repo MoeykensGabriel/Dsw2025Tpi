4525107492c000cc569d717593a7adfe6ccdfc98
MoeykensGabriel <moeykens.gabriel@gmail.com>
2025-06-21 15:49:04 -0300

    Se agregaron las entidades

--- Dsw2025Tpi.Domain/Entities/Order.cs
+++ Dsw2025Tpi.Domain/Entities/Order.cs
@@ -8,16 +8,25 @@ namespace Dsw2025Tpi.Domain.Entities
 {
     public class Order:EntityBase
     {
-
         public Guid CustomerId { get; set; }
         public DateTime Date {  get; set; }
         public string ShipiingAdress { get; set; }
         public string BillingAdress { get; set; }
         public string? Notes { get; set; }
-
         public OrderStatus Status { get; set; } = OrderStatus.PENDING;
-
-        public List<OrderItem> OrderItems { get; set; } = new();
+        public ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
         public decimal TotalAmount => OrderItems.Sum(item => item.Subtotal);
+
+        public Order() { }
+        public Order(Guid customerId, string shippingAddress, string billingAddress, List<OrderItem> items, string? notes = null)
+        {
+            CustomerId = customerId;
+            ShipiingAdress = shippingAddress;
+            BillingAdress = billingAddress;
+            Notes = notes;
+            OrderItems = items;
+            Date = DateTime.UtcNow;
+            Status = OrderStatus.PENDING;
+        }
     }
 }
