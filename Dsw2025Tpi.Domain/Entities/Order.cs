3b3f6c526930cd5989737aab6472e4e8ed023ccc
MoeykensGabriel <moeykens.gabriel@gmail.com>
2025-06-21 12:47:13 -0300

    Se agrego la entidad 'OrderItem' y se modificaron atributos de las entidades

--- Dsw2025Tpi.Domain/Entities/Order.cs
+++ Dsw2025Tpi.Domain/Entities/Order.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace Dsw2025Tpi.Domain.Entities
 {
-    internal class Order
+    public class Order
     {
+
+        public Guid CustomerId { get; set; }
+        public DateTime Date {  get; set; }
+        public string ShipiingAdress { get; set; }
+        public string BillingAdress { get; set; }
+        public string? Notes { get; set; }
+
+        public OrderStatus Status { get; set; } = OrderStatus.PENDING;
+
+        public List<OrderItem> OrderItems { get; set; } = new();
+        public decimal TotalAmount => OrderItems.Sum(item => item.Subtotal);
     }
 }
