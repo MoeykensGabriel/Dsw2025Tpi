4f7efa4d1bf58bc80b108b65d395f0b169e078f3
MoeykensGabriel <moeykens.gabriel@gmail.com>
2025-07-04 18:21:05 -0300

    Se realizaron cambios en Customer.cs

--- Dsw2025Tpi.Domain/Entities/Customer.cs
+++ Dsw2025Tpi.Domain/Entities/Customer.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace Dsw2025Tpi.Domain.Entities
 {
-    internal class Customers
-    {
-    }
+        public class Customer : EntityBase
+        {
+            public string? Email { get; set; }
+            public string? Name { get; set; }
+            public string? PhoneNumber { get; set; }
+
+            public Customer(string email, string name, string phoneNumber, Guid id) : base(id)
+            {
+                this.Email = email;
+                this.Name = name;
+                this.PhoneNumber = phoneNumber;
+            }
+
+            public Customer(string email, string name, string phoneNumber) : base()
+            {
+                this.Email = email;
+                this.Name = name;
+                this.PhoneNumber = phoneNumber;
+            }
+
+            public Customer() : base() { }
+        }
 }
