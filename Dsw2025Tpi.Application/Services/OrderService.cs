8be0aa8fa3c64a33e371f9e83658867ec7203fa2
MoeykensGabriel <moeykens.gabriel@gmail.com>
2025-07-21 23:03:39 -0300

    Refactorizacion en el metodo 'addOrder'

--- Dsw2025Tpi.Application/Services/OrderService.cs
+++ Dsw2025Tpi.Application/Services/OrderService.cs
@@ -15,134 +15,120 @@ namespace Dsw2025Tpi.Application.Services
 
         public async Task<OrderModel.Response> addOrder(OrderModel.Request order)
         {
-
-            if (String.IsNullOrWhiteSpace(order.ShippingAddress) || String.IsNullOrWhiteSpace(order.BillingAddress) || order.OrderItems == null)
-            {
+            if (string.IsNullOrWhiteSpace(order.ShippingAddress) || string.IsNullOrWhiteSpace(order.BillingAddress) || order.OrderItems == null)
                 throw new ArgumentException("Los datos ingresados de la orden no son válidos.");
-            }
 
             if (await _repository.First<Customer>(p => p.Id == order.CustomerId) == null)
-            {
                 throw new ArgumentException($"Cliente con el ID {order.CustomerId} no encontrado en la base de datos.");
-            }
-            else
+
+            OrderModel.OrderItemRequest itemDuplicate, itemNew;
+            var itemsOrderFinish = new List<OrderModel.OrderItemRequest>();
+
+            foreach (var q in order.OrderItems.ToList())
             {
-                OrderModel.OrderItemRequest itemDuplicate, itemNew;
-                var itemsOrderFinish = new List<OrderModel.OrderItemRequest>();
-                foreach (var q in order.OrderItems.ToList())
+                if (!itemsOrderFinish.Any())
                 {
-                    if (itemsOrderFinish.Any() == false)
-                    {
-                        itemsOrderFinish.Add(q);
-                    }
-                    else
-                    {
-                        if (itemsOrderFinish.Exists(p => p.ProductId == q.ProductId))
-                        {
-                            itemDuplicate = itemsOrderFinish.Find(p => p.ProductId == q.ProductId);
-                            itemsOrderFinish.Remove(itemDuplicate);
-
-                            var combinedQuantity = itemDuplicate.Quantity + q.Quantity;
-
-                            itemNew = new OrderModel.OrderItemRequest(
-                                        itemDuplicate.ProductId,
-                                         itemDuplicate.Name,
-                                         itemDuplicate.Description,
-                                         itemDuplicate.CurrentUnitPrice,
-                                        combinedQuantity
-                             );
-
-
-                            itemsOrderFinish.Add(itemNew);
-                        }
-                        else
-                        {
-                            itemsOrderFinish.Add(q);
-                        }
-                    }
+                    itemsOrderFinish.Add(q);
                 }
-                decimal TotalAmount = 0;
-                OrderItem itemInOrder;
-                List<OrderItem> allItems = new List<OrderItem>();
-                List<Product> allProductsUpdate = new List<Product>();
-                var allProducts = await _repository.GetAll<Product>();
-                var orderAdd = new Order();
-
-                foreach (var q in itemsOrderFinish)
+                else if (itemsOrderFinish.Exists(p => p.ProductId == q.ProductId))
                 {
-                    if (q.Quantity <= 0)
-                    {
-                        throw new ArgumentException("Cantidad de uno de los productos menor/igual a cero.");
-                    }
-                    if (!(allProducts.ToList().Exists(p => (p.Id == q.ProductId && p.Name == q.Name && p.CurrentUnitPrice == q.CurrentUnitPrice))))
-                    {
-                        throw new EntityNotFoundException($"No existe un producto que está en la orden con el ID, Nombre o precio indicado.");
-                    }
-                    foreach (Product p in allProducts)
-                    {
-                        if (p.Id == q.ProductId && p.Name == q.Name && p.CurrentUnitPrice == q.CurrentUnitPrice)
-                        {
-                            if (q.Quantity > p.StockQuantity)
-                            {
-                                throw new ArgumentException("No hay suficiente cantidad de productos para la orden.");
-                            }
-                            if (p.IsActive == false)
-                            {
-                                throw new ArgumentException("Existen productos inhabilitados en la orden.");
-                            }
+                    itemDuplicate = itemsOrderFinish.Find(p => p.ProductId == q.ProductId);
+                    itemsOrderFinish.Remove(itemDuplicate);
 
-                            itemInOrder = new OrderItem();
-                            itemInOrder.SkuProd = p.Sku;
-                            itemInOrder.ProductId = p.Id;
-                            itemInOrder.Quantity = q.Quantity;
-                            itemInOrder.UnitPrice = q.CurrentUnitPrice;
-                            itemInOrder.Subtotal = (q.CurrentUnitPrice * q.Quantity);
-                            itemInOrder.OrderId = orderAdd.Id;
+                    var combinedQuantity = itemDuplicate.Quantity + q.Quantity;
 
-                            TotalAmount += itemInOrder.Subtotal;
+                    itemNew = new OrderModel.OrderItemRequest(
+                        itemDuplicate.ProductId,
+                        itemDuplicate.Name,
+                        itemDuplicate.Description,
+                        itemDuplicate.CurrentUnitPrice,
+                        combinedQuantity
+                    );
 
-                            allItems.Add(itemInOrder);
-
-                            p.StockQuantity -= q.Quantity;
-                            allProductsUpdate.Add(p);
-
-                        }
-                    }
+                    itemsOrderFinish.Add(itemNew);
                 }
-
-                orderAdd.Date = DateTime.Now;
-                orderAdd.ShippingAddress = order.ShippingAddress;
-                orderAdd.BillingAddress = order.BillingAddress;
-                orderAdd.Notes = order.Notes;
-                orderAdd.TotalAmount = TotalAmount;
-                orderAdd.CustomerId = order.CustomerId;
-                orderAdd.OrderItems = allItems;
-                foreach (Product p in allProductsUpdate)
+                else
                 {
-                    await _repository.Update<Product>(p);
+                    itemsOrderFinish.Add(q);
                 }
-                foreach (OrderItem o in allItems)
-                {
-                    await _repository.Add(o);
-                }
-                await _repository.Add(orderAdd);
-                return new OrderModel.Response(
-                        Id: orderAdd.Id,
-                        Status: orderAdd.Status,
-                        TotalAmount: orderAdd.TotalAmount,
-                        Date: orderAdd.Date,
-                        ShippingAddress: orderAdd.ShippingAddress,
-                        BillingAddress: orderAdd.BillingAddress,
-                        Items: allItems.Select(item => new OrderModel.OrderItemResponse(
-                            ProductId: allProducts.First(p => p.Sku == item.SkuProd).Id, // o Guid.Empty si no querés resolverlo ahora
-                            Name: item.Product?.Name ?? "", // si hacés Include o lo resolvés aparte
-                            Quantity: item.Quantity,
-                            UnitPrice: item.UnitPrice,
-                            Subtotal: item.Subtotal
-                        )).ToList()
-                        );
             }
+
+            decimal TotalAmount = 0;
+            List<OrderItem> allItems = new List<OrderItem>();
+            List<Product> allProductsUpdate = new List<Product>();
+            var allProducts = await _repository.GetAll<Product>();
+            var orderAdd = new Order();
+
+            foreach (var q in itemsOrderFinish)
+            {
+                if (q.Quantity <= 0)
+                    throw new ArgumentException("Cantidad de uno de los productos menor/igual a cero.");
+
+                var product = allProducts.FirstOrDefault(p =>
+                    p.Id == q.ProductId &&
+                    p.Name == q.Name &&
+                    p.CurrentUnitPrice == q.CurrentUnitPrice
+                );
+
+                if (product == null)
+                    throw new EntityNotFoundException($"Producto con ID={q.ProductId}, Nombre={q.Name}, Precio={q.CurrentUnitPrice} no encontrado.");
+
+                if (!product.IsActive)
+                    throw new ArgumentException("Existen productos inhabilitados en la orden.");
+
+                if (q.Quantity > product.StockQuantity)
+                    throw new ArgumentException("No hay suficiente cantidad de productos para la orden.");
+
+                var itemInOrder = new OrderItem
+                {
+                    SkuProd = product.Sku,
+                    ProductId = product.Id,
+                    Quantity = q.Quantity,
+                    UnitPrice = q.CurrentUnitPrice,
+                    Subtotal = q.CurrentUnitPrice * q.Quantity,
+                    OrderId = orderAdd.Id
+                };
+
+                TotalAmount += itemInOrder.Subtotal;
+                allItems.Add(itemInOrder);
+
+                product.StockQuantity -= q.Quantity;
+                allProductsUpdate.Add(product);
+            }
+
+            orderAdd.Date = DateTime.Now;
+            orderAdd.ShippingAddress = order.ShippingAddress;
+            orderAdd.BillingAddress = order.BillingAddress;
+            orderAdd.Notes = order.Notes;
+            orderAdd.TotalAmount = TotalAmount;
+            orderAdd.CustomerId = order.CustomerId;
+            orderAdd.OrderItems = allItems;
+
+            foreach (var p in allProductsUpdate)
+                await _repository.Update<Product>(p);
+
+            foreach (var o in allItems)
+                await _repository.Add(o);
+
+            await _repository.Add(orderAdd);
+
+            return new OrderModel.Response(
+                Id: orderAdd.Id,
+                Status: orderAdd.Status,
+                TotalAmount: orderAdd.TotalAmount,
+                Date: orderAdd.Date,
+                ShippingAddress: orderAdd.ShippingAddress,
+                BillingAddress: orderAdd.BillingAddress,
+                Items: allItems.Select(item => new OrderModel.OrderItemResponse(
+                    ProductId: allProducts.First(p => p.Sku == item.SkuProd).Id,
+                    Name: item.Product?.Name ?? "",
+                    Quantity: item.Quantity,
+                    UnitPrice: item.UnitPrice,
+                    Subtotal: item.Subtotal
+                )).ToList()
+            );
         }
+
 
         // no acoplar la capa del dominio, debo devolver OrderModel.Respons
         public async Task<IEnumerable<OrderModel.Response>> GetAllOrders()
