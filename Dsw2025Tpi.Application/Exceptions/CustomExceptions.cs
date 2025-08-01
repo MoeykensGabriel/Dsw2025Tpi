a78db25000952dd647300ca2faf6da3bf88fa9c1
MoeykensGabriel <moeykens.gabriel@gmail.com>
2025-08-01 19:54:18 -0300

    Clase 'CustomExceptions' con excepciones completas

--- Dsw2025Tpi.Application/Exceptions/CustomExceptions.cs
+++ Dsw2025Tpi.Application/Exceptions/CustomExceptions.cs
@@ -6,24 +6,34 @@ using System.Threading.Tasks;
 
 namespace Dsw2025Tpi.Application.Exceptions
 {
+    // 404 para not found
     public class EntityNotFoundException: Exception
     {
         public EntityNotFoundException(string message):base(message) { }
     }
 
+    // 409 para conflictos por ej para datos duplicados
     public class DuplicatedEntityException: Exception 
     {
         public DuplicatedEntityException(string message): base(message) { }
     }
 
+    // 400 bad request
     public class BadRequestException: Exception
     {
         public BadRequestException(string message):base(message) { }
     }
 
+    // 401 para unauthorized
     public class  UnauthorizedException: Exception
     {
-        public UnauthorizedException(string message) : base{ }
+        public UnauthorizedException(string message) : base(message){ }
     }
 
+    // 409 para conflictos generales
+    public class ConflictException : Exception
+    {
+        public ConflictException(string message):base(message) { }
+    }        
+    
 }
