fee861a990446acbeb3877c678c3ac538ad0d2f7
Luciano Lagoria <Luciano.LagoriaVillagran@alu.frt.utn.edu.ar>
2025-11-10 12:20:58 -0300

    Add null in record register model

--- Dsw2025Tpi.Application/Dtos/RegisterModel.cs
+++ Dsw2025Tpi.Application/Dtos/RegisterModel.cs
@@ -6,6 +6,6 @@ using System.Threading.Tasks;
 
 namespace Dsw2025Tpi.Application.Dtos;
 
-public record RegisterModel(string Username, string Email, string Password, string Role);
+public record RegisterModel(string Username, string Email, string Password, string? Role);
 
 
