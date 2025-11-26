58db83034cad90231145552b299a0592364b9312
Luciano Lagoria <Luciano.LagoriaVillagran@alu.frt.utn.edu.ar>
2025-11-26 01:43:14 -0300

    Fix message product

--- Dsw2025Tpi.Application/Dtos/ProductModel.cs
+++ Dsw2025Tpi.Application/Dtos/ProductModel.cs
@@ -10,7 +10,7 @@ namespace Dsw2025Tpi.Application.Dtos;
 public record ProductModel
 {
     public record Request(
-    string Sku, string InternalCode, string Name, string Description, decimal CurrentUnitPrice,
+    string Sku, string InternalCode, string Name, string? Description, decimal CurrentUnitPrice,
     int StockQuantity, string? ImageUrl, bool IsActive
     );
 
@@ -19,7 +19,7 @@ public record ProductModel
     string Sku,
     string InternalCode,
     string Name,
-    string? Description,
+    string Description,
     decimal CurrentUnitPrice,
     int StockQuantity,
     bool IsActive, 
